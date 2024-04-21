c5be875a34b4d67f8c97357054e918ad8868d514
FranklinRicaldiCastro <fricaldic@gmail.com>
2024-04-20 23:48:53 -0500

    first commit

---
+++ src/customer/components/HomeCarosel/MainCarouselData.js
@@ -0,0 +1,14 @@
+export const mainCarouselData=[
+  {
+    image:"https://image.lexica.art/full_webp/25a660eb-b52c-4740-9932-614f113ca59d",
+    path:"carousel_01/carrito_de_compras"
+  },
+  {
+    image:"https://image.lexica.art/full_webp/70975223-83ce-4380-b1d9-eb871b57fb70",
+    path:"carousel_02/vendedora"
+  },
+  {
+    image:"https://image.lexica.art/full_webp/936adc7a-10ac-49f3-97cd-1049ef7d9a98",
+    path:"carousel_03/cliente_observando_productos"
+  }
+]
