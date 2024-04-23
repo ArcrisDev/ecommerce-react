7c76793969a269055836443d9cd3856e33669b59
FranklinRicaldiCastro <fricaldic@gmail.com>
2024-04-22 19:51:27 -0500

    Actualizando el homeSectionCarosel

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,37 +1,73 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-import { Button } from '@mui/material';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { Button } from "@mui/material";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
 const HomeSectionCarosel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
-  }
+  };
 
-  const items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
+  const slidePrev=()=>setActiveIndex(activeIndex-1);
+  const slideNext=()=>setActiveIndex(activeIndex+1)
+
+  const syncActiveIndex=({item})=>setActiveIndex(item)
+
+  const items = [1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
   return (
-    <div className='px-4 lg:px-8'>
-      <div className='relative p-5 border'>
+    <div className="border">
+      <div className="relative p-5 ">
         <AliceCarousel
           items={items}
           disableButtonsControls
-          infinite
           responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
         />
-        <Button variant='contained' className='z-50 bg-white' sx={{ position: 'absolute', top: '8rem', right: '0rem', transform: 'translateX(50%) rotate(90deg)', bgcolor: 'white' }} arial-label='next'>
-          <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+        <Button
+          variant="contained"
+          className="z-50 bg-white"
+          onClick={slideNext}
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+          arial-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
         </Button>
 
-        <Button variant='contained' className='z-50 bg-white' sx={{ position: 'absolute', top: '8rem', left: '0rem', transform: 'translateX(-50%) rotate(-90deg)', bgcolor: 'white' }} arial-label='next'>
-          <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+        <Button
+          onClick={slidePrev}
+          variant="contained"
+          className="z-50 bg-white"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "0rem",
+            transform: "translateX(-50%) rotate(-90deg)",
+            bgcolor: "white",
+          }}
+          arial-label="next"
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCarosel
+export default HomeSectionCarosel;
